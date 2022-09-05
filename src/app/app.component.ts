8239f962c6ffe67c9008493af71c6283ba020ab5
Amal Ahmed <dev.amal.ahmed@gmail.com>
2022-09-05 12:07:10 +0200

    add scrolly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+  activeSection = 'home';
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any): void {
+    const offset = window.scrollY;
+    if (offset < 390) {
+      this.activeSection = 'home';
+    } else if (offset > 390 && offset < 900) {
+      this.activeSection = 'about';
+    } else if (offset >= 900 && offset < 1600) {
+      this.activeSection = 'portfolio';
+    } else if (offset >= 1600 && offset < 2200) {
+      this.activeSection = 'certificate';
+    } else {
+      this.activeSection = 'contact';
+    }
+  }
 }
--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @Input() activeLink = 'home';
   constructor() {}
+
+  onaLinkClicked(linkName = ''): void {
+    this.activeLink = linkName;
+  }
 }
